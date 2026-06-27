3a8a2ccf287a96dab5156a5af8012696f35491a5
Kora <471209058@qq.com>
2026-06-27 08:58:37 -0400

    fix: guard Taro.cloud.init to prevent application mount crash when cloud SDK is not loaded

--- apps/miniprogram/src/app.tsx
+++ apps/miniprogram/src/app.tsx
@@ -4,7 +4,7 @@ import "./app.scss";
 
 class App extends Component<PropsWithChildren<{}>> {
 	componentDidMount() {
-		if (process.env.TARO_ENV === "weapp") {
+		if (process.env.TARO_ENV === "weapp" && typeof Taro.cloud !== "undefined") {
 			Taro.cloud.init({
 				traceUser: true,
 			});
